b045f09b23f0ccc3db45b7c8a729c42b3f1f09db
spchee <53122886+spee-chee@users.noreply.github.com>
2020-06-22 22:36:06 +0100

    Initial upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import discord
+from discord.ext import commands
+import datetime
+client = commands.Bot(command_prefix = "?")
+
+@client.event
+async def on_ready():
+    print("bot is ready")
+
+@client.command()
+async def countdown(ctx):
+
+    time = datetime.datetime(2020, 8, 13,7,30) - datetime.datetime.now()
+    days = time.days
+    hours = time.seconds//3600
+    minutes = (time.seconds%3600)//60
+    embed=discord.Embed(title="Time left until A-level Results", color=0xe32323)
+    embed.add_field(name="Days", value=days, inline=True)
+    embed.add_field(name="Hours", value=hours, inline=True)
+    embed.add_field(name="Minutes", value=minutes, inline=True)
+    embed.set_footer(text="A-Level Coutndown Bot")
+    await ctx.send(embed = embed)
+
+token = "NzI0NzA5MjQ1NzUyMTgwNzk2.XvEJLA.IpZIDsl-k06gVqucX9NylCYRC1s"
+client.run(token)
